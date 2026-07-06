cade07e590e3d3065137626728f5e68255db0ef0
vladu224 <vlad.andone04@gmail.com>
2026-07-06 16:15:04 +0300

    Remove Calculator component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
         <Hero />
         <Services />
         <Pricing />
-        <Calculator />
         <Process />
         <Testimonials />
         <Faq /> 
