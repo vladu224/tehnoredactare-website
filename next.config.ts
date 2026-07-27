cdd457494888b94651ae02f57ca40e712444e77c
vladu224 <vlad.andone04@gmail.com>
2026-07-27 18:02:32 +0300

    fix: change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  allowedDevOrigins: ["192.168.0.29"]
+  allowedDevOrigins: [
+    "192.168.0.29",
+    "192.168.1.137",
+  ]
 };
 
 export default nextConfig;
