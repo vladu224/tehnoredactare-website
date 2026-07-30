b0ae7e0d675c8a427273e049c70f16646b8ee22a
vladu224 <vlad.andone04@gmail.com>
2026-07-30 20:00:59 +0300

    Remove unused Tinos font import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Fraunces, Inter, Tinos } from "next/font/google";
+import { Fraunces, Inter } from "next/font/google";
 import "./globals.css";
 
 const fraunces = Fraunces({
